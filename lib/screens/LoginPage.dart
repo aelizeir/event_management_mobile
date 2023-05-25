1bd42ff6fb979935485cd151cd00ab70630cb548
aelizeir <aligutierrez1016@gmail.com>
2023-05-25 21:58:17 +0800

    Initial Commit

---
+++ lib/screens/LoginPage.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'HomePage.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String id = '';
+  String password = '';
+  TextEditingController idController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        key: formKey,
+        child: SafeArea(
+          child: SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Spacer(),
+                  Text(
+                      "USTP Event",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                      "Log in",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const Spacer(),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[350],
+                        borderRadius: const BorderRadius.all(Radius.circular(8))
+                    ),
+                    child: TextFormField(
+                      controller: idController,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter ID',
+                        border: InputBorder.none,
+                        prefixIcon: Icon(Icons.person),
+                        ),
+                      validator: (value) {
+                        return (value == '') ? 'Please enter value' : null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[350],
+                        borderRadius: const BorderRadius.all(Radius.circular(8))
+                    ),
+                    child: TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter Password',
+                        border: InputBorder.none,
+                        prefixIcon: Icon(Icons.lock),
+                        suffixIcon: Icon(Icons.remove_red_eye)
+                      ),
+                      validator: (value) {
+                        return (value == '') ? 'Please enter value' : null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: const Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.yellow),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)))
+
+                    ),
+                    onPressed: () {
+                      var isFormValid = formKey.currentState!.validate();
+                      if (isFormValid) {
+                        if(idController.text == 'alley@test.com') {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+                        }
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                              content: Text('Invalid Credentials')),
+                        );
+                      }
+                    },
+                    child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                        width: Get.width * 0.6,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            borderRadius: const BorderRadius.all(Radius.circular(40))
+                        ),
+                        child: const Text(
+                          "LOG IN",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                  ),
+                  ),
+                  const Spacer(),
+                  // Row(
+                  //   mainAxisAlignment: MainAxisAlignment.center,
+                  //   children: [
+                  //     Text('Do not have an account? '),
+                  //     GestureDetector(
+                  //       onTap: () => Get.to(() => RegisterScreen(), transition: Transition.cupertino),
+                  //       child: Text('Create One.',
+                  //       style: TextStyle(
+                  //         color: Colors.blueGrey,
+                  //         fontWeight: FontWeight.w600,
+                  //         fontSize: 14,
+                  //       ),),
+                  //     ),
+                  //   ],
+                  // )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+
+        // appBar: AppBar(
+        //   backgroundColor: Colors.black,
+        //   centerTitle: true,
+        //   elevation: 0,
+        //   title: const Text(
+        //     'Login',
+        //     style: TextStyle(
+        //       fontSize: 20,
+        //       fontWeight: FontWeight.bold,
+        //     ),
+        //   ),
+        // ),
+        // body: Padding(
+        //   padding: const EdgeInsets.symmetric(horizontal: 20),
+        //   child: Column(
+        //     children: [
+        //       const SizedBox(
+        //         height: 20,
+        //       ),
+        //       TextField(
+        //         decoration: const InputDecoration(
+        //           hintText: 'Enter your email',
+        //         ),
+        //         onChanged: (value) {
+        //           _email = value;
+        //         },
+        //       ),
+        //       const SizedBox(
+        //         height: 30,
+        //       ),
+        //       TextField(
+        //         obscureText: true,
+        //         decoration: const InputDecoration(
+        //           hintText: 'Enter your password',
+        //         ),
+        //         onChanged: (value) {
+        //           _password = value;
+        //         },
+        //       ),
+        //       const SizedBox(
+        //         height: 30,
+        //       ),
+        //       ElevatedButton(
+        //           onPressed:() {
+        //             if(_email == 'alley@test.com') {
+        //               HomePage();
+        //             } else if (_email == 'rose@test.com') {
+        //               HomePage2();
+        //             } else if (_email == 'alex@test.com') {
+        //               HomePage3();
+        //             }
+        //           },
+        //         child: Text("Log In"),
+        //       ),
+        //     ],
+        //   ),
+        // )
+    );
+  }
+}
