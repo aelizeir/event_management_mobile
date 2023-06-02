a2a69bc69e9537f1f233e0ecb56995b0a573bfa2
Lloyd Anthony Salas <106375944+Tensa11@users.noreply.github.com>
2023-06-02 23:09:13 +0800

    Static UI Update for attendance.dart
    Need Back-end

--- lib/screens/tabs/attendance.dart
+++ lib/screens/tabs/attendance.dart
@@ -1,12 +1,156 @@
 import 'package:flutter/material.dart';
 
-class AttendancePage extends StatelessWidget {
+class AttendancePage extends StatefulWidget {
   const AttendancePage({Key? key}) : super(key: key);
 
   @override
+  State<AttendancePage> createState() => _AttendancePageState();
+}
+
+class _AttendancePageState extends State<AttendancePage> {
+  Widget listItem({required Map items, required String key}) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 35, vertical: 15),
+      padding: const EdgeInsets.all(20),
+      height: 170,
+      decoration: BoxDecoration(
+        color: const Color(0xFFFFFFFF),
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            offset: const Offset(0, 3),
+            blurRadius: 15,
+          ),
+        ],
+      ),
+      child: const Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.check_circle,
+            size: 70,
+            color: Colors.green,
+          ),
+          SizedBox(width: 20),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Wed, Apr 28 • 5:30 PM',
+                  style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400, color: Colors.blue),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  'Week Of Wellness Opening Sample Event ',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Student-ID: 2020301936',
+                  style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400, color: Colors.black),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      size: 16,
+                      color: Color(0xFF747688),
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      'USTP Memorial Hall',
+                      style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: Color(0xFF747688)),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('This is attendance page'),
+    List<Map<dynamic, dynamic>> attendance = [
+      // Replace this with your actual list of data
+      {
+        'key': 'your_key_value_1',
+        // other data fields...
+      },
+      {
+        'key': 'your_key_value_1',
+        // other data fields...
+      },
+      {
+        'key': 'your_key_value_1',
+        // other data fields...
+      },
+      {
+        'key': 'your_key_value_1',
+        // other data fields...
+      },
+      {
+        'key': 'your_key_value_1',
+        // other data fields...
+      },
+      {
+        'key': 'your_key_value_1',
+        // other data fields...
+      },
+      {
+        'key': 'your_key_value_1',
+        // other data fields...
+      },
+      {
+        'key': 'your_key_value_2',
+        // other data fields...
+      },
+      {
+        'key': 'your_key_value_2',
+        // other data fields...
+      },
+      // add more items as needed
+    ];
+
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: ListView.builder(
+        itemCount: attendance.length, // Replace 'yourDataList' with the actual list of data
+        itemBuilder: (BuildContext context, int index) {
+          Map<dynamic, dynamic> items = attendance[index];
+          String key = items['key']; // Replace 'key' with the actual key field in your data
+          return SingleChildScrollView(
+            child: listItem(items: items, key: key),
+          );
+        },
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: const Color(0xFFFFFFFF),
+      elevation: 0,
+      leading: const Icon(
+        Icons.arrow_back_outlined,
+        color: Colors.black,
+        size: 30,
+      ),
+      title: const Text(
+        'Attendance Log',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 21,
+          fontFamily: 'Poppins',
+          fontWeight: FontWeight.w600,
+        ),
+      ),
     );
   }
 }
